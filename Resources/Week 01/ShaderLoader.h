a4fc08f05e91ef57f001c5fff2d17537ccaa1184
ferp132 <37201345+ferp132@users.noreply.github.com>
2018-08-13 16:09:57 +1200

    added resources

---
+++ Resources/Week 01/ShaderLoader.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\freeglut\freeglut.h"
+#include <iostream>
+#include <map>
+
+
+class ShaderLoader
+{
+	private:
+
+		std::string ReadShader(char *filename);
+		GLuint CreateShader(GLenum shaderType,
+			std::string source,
+			char* shaderName);
+
+	public:
+		ShaderLoader(void);
+		~ShaderLoader(void);
+		GLuint CreateProgram(char* VertexShaderFilename,
+			char* FragmentShaderFilename);
+
+
+		std::map<int, char*> VShaderMap;
+		std::map<int, char*> FShaderMap;
+		std::map<int, char*> ProgramMap;
+};
